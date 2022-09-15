b753f0d6927f968cd307d31681296da906f2fdac
Cesar Camacho <cesarcamacho760@gmail.com>
2022-09-15 13:36:53 -0700

    fixes

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,1 +1,10 @@
-(()=>{"use strict";const e=document.getElementById("content");(()=>{const t=document.createElement("div");t.setAttribute("id","header"),t.innerText="Todo List",e.appendChild(t)})()})();
+(() => {
+  "use strict";
+  const e = document.getElementById("content");
+  (() => {
+    const t = document.createElement("div");
+    t.setAttribute("id", "header"),
+      (t.innerText = "Todo List"),
+      e.appendChild(t);
+  })();
+})();
